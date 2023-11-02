990ca663fb69f4dd3b09f82cd71bda49a86421ad
ikbakk <iqbalfirdaus05@gmail.com>
2023-11-02 17:34:28 +0800

    remove logger api list

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
-    logger: ['log', 'warn', 'error'],
+    logger: ['error', 'warn'],
     bodyParser: true,
   });
 
   app.enableCors({
-    origin: '*',
+    origin: process.env.CORS_ORIGIN,
     credentials: true,
   });
 
-  await app.listen(3333);
+  await app.listen(3333, () => console.log('Server is running'));
 }
 bootstrap();
