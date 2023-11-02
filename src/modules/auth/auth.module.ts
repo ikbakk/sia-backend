6dc79bf540194ddc2ba7cadeb8a284795cccab8f
ikbakk <iqbalfirdaus05@gmail.com>
2023-11-02 17:12:40 +0800

    increase jwt duration

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -12,7 +12,7 @@ import { LecturersModule } from '../lecturers/lecturers.module';
     JwtModule.register({
       global: true,
       secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: '120s' },
+      signOptions: { expiresIn: '1d' },
     }),
   ],
   controllers: [AuthController],
