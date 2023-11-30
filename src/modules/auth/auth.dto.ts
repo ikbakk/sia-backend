621730de223ff1490d0be17865a5e289cd5ae077
ikbakk <iqbalfirdaus05@gmail.com>
2023-11-30 10:04:44 +0800

    edit auth dto

--- src/modules/auth/auth.dto.ts
+++ src/modules/auth/auth.dto.ts
@@ -3,9 +3,9 @@ import { IsNotEmpty, IsString } from 'class-validator';
 export class AuthStudentDto {
   @IsString()
   @IsNotEmpty()
-  studentID: string;
+  password: string;
 
   @IsString()
   @IsNotEmpty()
-  password: string;
+  studentID: string;
 }
